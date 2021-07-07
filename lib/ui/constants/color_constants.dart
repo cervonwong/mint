724b398ee8a116c8e6dcc84d076a95434e7292bf
Cervon Wong <tcrammy@gmail.com>
2021-07-07 18:24:29 +0800

    Create color constants

---
+++ lib/ui/constants/color_constants.dart
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 Cervon Wong and Lee I-Shiang
+ */
+
+import 'dart:ui';
+
+class ColorConstants {
+  ColorConstants._(); // Prevent instantiation with private constructor.
+
+  static const blackPrimary = Color(0xFF1C1917);
+  static const blackSecondary = Color(0xA61C1917); // 65%.
+  static const blackTertiary = Color(0x661C1917); // 40%.
+
+  static const whitePrimary = Color(0xFFFFFFFF);
+  static const whiteSecondary = Color(0xB3FFFFFF); // 70%.
+
+  static const greenPrimary = Color(0xFF468162);
+  static const greenLightOverlay = Color(0x0D468162); // 5%.
+  static const greenOverlay = Color(0x1A468162); // 10%.
+  static const greenLightBorder = Color(0x33468162); // 20%.
+  static const greenBorder = Color(0x4D468162); // 30%.
+  static const greenDisabled = Color(0x66468162); // 40%.
+  static const greenInactive = Color(0x80468162); // 50%.
+  static const greenDark = Color(0xFF2A6341);
+
+  static const ivoryPrimary = Color(0xFFFFFCEB);
+  static const ivoryOverlay = Color(0xEBFFFCEB); // 92%.
+}
