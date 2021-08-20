31a457f00117203a06e3a3d59abdc88f1b5655e1
Cervon Wong <tcrammy@gmail.com>
2021-08-20 10:26:35 +0800

    Create classes to show MWE of architecture

---
+++ lib/controller/example_recipes_controller.dart
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2021 Cervon Wong and Lee I-Shiang
+ */
+
+import 'package:flutter/foundation.dart';
+
+class ExampleRecipesController extends ChangeNotifier {}
---
+++ lib/ui/example_widget.dart
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2021 Cervon Wong and Lee I-Shiang
+ */
+
+import 'package:flutter/material.dart';
+
+class ExampleWidget extends StatelessWidget {
+  const ExampleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
