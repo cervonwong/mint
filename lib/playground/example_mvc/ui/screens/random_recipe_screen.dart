92bfa944a7539bb1e5d7260bbe35d3aca6e64cca
Cervon Wong <tcrammy@gmail.com>
2021-08-21 11:22:01 +0800

    Modify wording of example MVC app to show it not part of final product

--- lib/playground/example_mvc/ui/screens/random_recipe_screen.dart
+++ lib/playground/example_mvc/ui/screens/random_recipe_screen.dart
@@ -16,11 +16,15 @@ class RandomRecipeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Random recipe generator!')),
+      appBar: AppBar(
+          title: Text('PLAYGROUND EXAMPLE TO DEMONSTRATE MVC ARCHITECTURE')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('Random Recipe Generator! '
+                '(This is not a functionality of the actual product)'),
+            SizedBox(height: 32.0),
             RecipeLabel(),
             SizedBox(height: 64.0),
             // This is usually how we add spacing between widgets.
