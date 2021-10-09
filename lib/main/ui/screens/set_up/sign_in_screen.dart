1a3f998ad0c08557457fb29048734adc9e38fd29
Cervon Wong <tcrammy@gmail.com>
2021-10-09 22:35:26 +0800

    Prevent clipping at small heights, prefer selectable texts

--- lib/main/ui/screens/set_up/sign_in_screen.dart
+++ lib/main/ui/screens/set_up/sign_in_screen.dart
@@ -23,58 +23,60 @@ class SignInScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(
               horizontal: Provider.of<LayoutController>(context).wideMargin,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Sign In',
-                  textAlign: TextAlign.center,
-                  style: Provider.of<TextThemeController>(context).headline4,
-                ),
-                SizedBox(height: 6.0),
-                Text(
-                  'Please pass your device to your instructor '
-                  'to help you sign in.',
-                  textAlign: TextAlign.center,
-                  style: Provider.of<TextThemeController>(context)
-                      .subtitle7
-                      .copyWith(
-                        color: ColorConstants.blackSecondary,
-                      ),
-                ),
-                SizedBox(height: 8.0),
-                Icon(Icons.volume_up),
-                SizedBox(height: 48.0),
-                Text(
-                  'Email',
-                  style: Provider.of<TextThemeController>(context).subtitle7,
-                ),
-                SizedBox(height: 4.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter email',
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SelectableText(
+                    'Sign In',
+                    textAlign: TextAlign.center,
+                    style: Provider.of<TextThemeController>(context).headline4,
                   ),
-                ),
-                SizedBox(height: 24.0),
-                Text(
-                  'Password',
-                  style: Provider.of<TextThemeController>(context).subtitle7,
-                ),
-                SizedBox(height: 4.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter password',
+                  SizedBox(height: 6.0),
+                  SelectableText(
+                    'Please pass your device to your instructor '
+                    'to help you sign in.',
+                    textAlign: TextAlign.center,
+                    style: Provider.of<TextThemeController>(context)
+                        .subtitle7
+                        .copyWith(
+                          color: ColorConstants.blackSecondary,
+                        ),
                   ),
-                ),
-                SizedBox(height: 32.0),
-                ElevatedButton7(
-                  onPressed: () {},
-                  child: Text(
-                    'Continue',
+                  SizedBox(height: 8.0),
+                  Icon(Icons.volume_up),
+                  SizedBox(height: 48.0),
+                  SelectableText(
+                    'Email',
+                    style: Provider.of<TextThemeController>(context).subtitle7,
                   ),
-                ),
-              ],
+                  SizedBox(height: 4.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter email',
+                    ),
+                  ),
+                  SizedBox(height: 24.0),
+                  SelectableText(
+                    'Password',
+                    style: Provider.of<TextThemeController>(context).subtitle7,
+                  ),
+                  SizedBox(height: 4.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter password',
+                    ),
+                  ),
+                  SizedBox(height: 32.0),
+                  ElevatedButton7(
+                    onPressed: () {},
+                    child: Text(
+                      'Continue',
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
