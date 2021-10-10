327c8de47fa284952da843f64b2790036efcf60f
Cervon Wong <tcrammy@gmail.com>
2021-10-10 17:32:41 +0800

    Prevent external modification to list of steps in `Recipe` class

--- lib/main/models/recipe.dart
+++ lib/main/models/recipe.dart
@@ -2,11 +2,16 @@
  * Copyright (C) 2021 Cervon Wong and Lee I-Shiang
  */
 
+import 'dart:collection';
+
 import 'package:meta/meta.dart';
+import 'package:mint/main/models/step.dart';
 
 @immutable
 class Recipe {
-  final List steps;
+  // UnmodifiableListView prevents list to be edited externally.
+  UnmodifiableListView<Step> get steps => UnmodifiableListView(_steps);
+  final List<Step> _steps;
 
-  Recipe({required this.steps});
+  Recipe({required List<Step> steps}) : _steps = steps.toList();
 }
