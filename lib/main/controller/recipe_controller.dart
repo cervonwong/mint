d9f81b4dec15497cd192fe7479cdc8581ef7d48a
Cervon Wong <tcrammy@gmail.com>
2021-10-10 17:37:32 +0800

    Fix incorrect types

--- lib/main/controller/recipe_controller.dart
+++ lib/main/controller/recipe_controller.dart
@@ -9,17 +9,16 @@ import 'package:mint/main/models/recipe.dart';
 import '../models/step.dart';
 
 class RecipeController extends ChangeNotifier {
+  Recipe get recipe => _recipe;
   late Recipe _recipe;
 
-  get recipe => _recipe;
-
-  Future<void> requestRecipe({id: String}) async {
+  Future<void> requestRecipe({required String id}) async {
     _recipe = await _getRecipe(id: id);
     notifyListeners();
   }
 }
 
-Future<Recipe> _getRecipe({id: String}) async {
+Future<Recipe> _getRecipe({required String id}) async {
   var collection = FirebaseFirestore.instance.collection('recipes');
   var querySnapshot = await collection.doc(id).get();
 
