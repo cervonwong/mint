96f75194edd1e50bdfb4afc174a1217b0b761c75
Cervon Wong <tcrammy@gmail.com>
2021-10-31 11:38:29 +0800

    Define models for preparation instructions

---
+++ lib/main/models/preparation_instructions.dart
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2021 Cervon Wong and Lee I-Shiang
+ */
+
+import 'dart:collection';
+
+import 'package:meta/meta.dart';
+
+@immutable
+class PreparationInstructions {
+  UnmodifiableListView<PreparationStep> get steps {
+    return UnmodifiableListView(_steps);
+  }
+
+  final List<PreparationStep> _steps;
+
+  PreparationInstructions({
+    required List<PreparationStep> steps,
+  }) : _steps = steps.toList();
+}
+
+@immutable
+class PreparationStep {
+  final String instruction;
+  final String? imageUrl; // Can be null if instructor don't provide image.
+
+  PreparationStep({
+    required this.instruction,
+    this.imageUrl,
+  });
+}
