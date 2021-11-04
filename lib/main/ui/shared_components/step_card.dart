90409917008912166e5e6000e28ba21fc22eb3db
Cervon Wong <tcrammy@gmail.com>
2021-11-04 09:22:57 +0800

    Fix step card too close to button when max scrolled in tight vertical settings

--- lib/main/ui/shared_components/step_card.dart
+++ lib/main/ui/shared_components/step_card.dart
@@ -96,63 +96,66 @@ class StepCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: ColorConstants.whitePrimary,
-        border: Border.all(color: borderColor),
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: imageUrl == null
-                ? null
-                : ClipRRect(
-                    borderRadius: BorderRadius.circular(12.0),
-                    child: Image.network(
-                      imageUrl!,
-                      fit: BoxFit.fitWidth,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: ColorConstants.whitePrimary,
+          border: Border.all(color: borderColor),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: imageUrl == null
+                  ? null
+                  : ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: Image.network(
+                        imageUrl!,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16.0,
+                right: 16.0,
+                bottom: 16.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: '',
+                      children: instruction
+                          .split(' ')
+                          .map((word) => TextSpan(
+                                text: word + ' ',
+                                style: double.tryParse(word) == null
+                                    ? ThemeConstants.headline6
+                                    : ThemeConstants.headline6.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                        color: ColorConstants.greenPrimary,
+                                      ),
+                              ))
+                          .toList(),
                     ),
                   ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-              bottom: 16.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                RichText(
-                  text: TextSpan(
-                    text: '',
-                    children: instruction
-                        .split(' ')
-                        .map((word) => TextSpan(
-                              text: word + ' ',
-                              style: double.tryParse(word) == null
-                                  ? ThemeConstants.headline6
-                                  : ThemeConstants.headline6.copyWith(
-                                      fontWeight: FontWeight.bold,
-                                      color: ColorConstants.greenPrimary,
-                                    ),
-                            ))
-                        .toList(),
+                  const SizedBox(height: 12.0),
+                  ListenButton(
+                    id: instruction,
+                    text: instruction,
+                    labelType: LabelType.instruction,
                   ),
-                ),
-                const SizedBox(height: 12.0),
-                ListenButton(
-                  id: instruction,
-                  text: instruction,
-                  labelType: LabelType.instruction,
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
